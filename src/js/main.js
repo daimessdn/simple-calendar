6b01c232da9ea3a0e8a0a599a96f09f4ea974e47
Dimas Wihandono <daimessdn@gmail.com>
2021-02-26 13:21:28 +0700

    added current highlights and description input

--- src/js/main.js
+++ src/js/main.js
@@ -11,13 +11,19 @@ if (calendar == null) {
 }
 
 const updateCalendar = () => {
-  calendarOutput.innerHTML = `<thead><th>Name</th><th>Date</th><th>Time</th></thead>`;
+  calendarOutput.innerHTML = `<thead><th>Name</th><th>Date</th></thead>`;
   
   calendar.forEach((event) => {
     if (new Date(`${event.date} ${event.time}`) >= new Date()) {
-      calendarOutput.innerHTML += `<tr id="${event.id}"><td>${event.event}</td>
-                       <td>${event.date}</td>
-                       <td>${event.time}</td>
+      calendarOutput.innerHTML += `<tr id="${event.id}"" class="${
+                        new Date(event.date + ' ' + event.time) - new Date() <= 864e5 ? "current" : ""}
+                       ">
+                       <td title="${event.event}"
+                           ondblclick="this.children[1].style.display = this.children[1].style.display === 'none' ? 'block' : 'none'">
+                           ${event.event}<br />
+                           <span class="description" style="display: none">${event.description}<span>
+                       </td>
+                       <td>${event.date} ${event.time}</td>
                        <td class="delete" onclick="deleteCalendar(this.parentElement.id)">delete</td></tr>`;
     }
   });
@@ -38,11 +44,11 @@ const generateId = (length) => {
 const deleteCalendar = (id) => {
   console.log(id);
 
-  const finalCalendar = calendar.filter(cal => {
+  calendar = calendar.filter(cal => {
     return cal.id != id
   });
 
-  localStorage.setItem(CALENDAR_LOCAL_KEY, JSON.stringify(finalCalendar));
+  localStorage.setItem(CALENDAR_LOCAL_KEY, JSON.stringify(calendar));
   updateCalendar();
 }
 
@@ -55,14 +61,16 @@ calendarForm.addEventListener("submit", (event) => {
     id: generateId(25),
     event: event.target.event.value,
     date: event.target.date.value,
-    time: event.target.time.value
+    time: event.target.time.value,
+    description: event.target.description.value
   });
 
   localStorage.setItem(CALENDAR_LOCAL_KEY, JSON.stringify(calendar));
   updateCalendar();
   [event.target.event.value,
    event.target.date.value,
-   event.target.time.value] = ["", "", ""];
+   event.target.time.value,
+   event.target.description.value] = ["", "", "", ""];
 
    event.target.event.focus();
 });
